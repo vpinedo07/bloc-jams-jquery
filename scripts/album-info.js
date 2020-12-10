e381d5689250e7cc16375c5d65ab4ade7224b958
vpinedo07 <victor.pf@pachuca.tecnm.mx>
2020-12-09 20:16:17 -0600

    Controles de Play/Pause completos

--- scripts/album-info.js
+++ scripts/album-info.js
@@ -3,4 +3,6 @@
     $('#album-cover-art').attr('src', album.albumArtUrl);
     $('.artist').text(album.artist);
     $('#release-info').text(album.releaseInfo);
+
+    $('#song-number-column').text(album.songs[0].title);
 }
