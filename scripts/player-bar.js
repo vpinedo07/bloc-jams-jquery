11ec793e20b357b589e22c0c7cd7f989bd08daf7
vpinedo07 <victor.pf@pachuca.tecnm.mx>
2020-12-14 19:14:30 -0600

    Time Sliders

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -24,6 +24,26 @@
 
     });
 
+    $('#time-control input').on('input', function (event) {
+        player.skipTo(event.target.value);
+    });
+
+    setInterval(() => {
+        //console.log("Acción");
+        if (player.playState !== 'playing') {
+            return;
+        }
+
+        const currentTime = player.getTime();
+        const duration = player.getDuration();
+        const percent = (currentTime / duration) * 100;
+
+        $('#time-control .current-time').text(currentTime);
+
+        $('#time-control input').val(percent);
+
+    }, 1000);
+
     $('button#previous').on('click', function () {
         if (player.playState === 'playing') {
 
